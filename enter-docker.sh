e4e3b90ab3ac3b1316575bf4490af49fa088a63e
oosman <saeedoosman@johndeere.com>
2023-08-10 10:24:03 -0500

    wip

--- enter-docker.sh
+++ enter-docker.sh
@@ -31,6 +31,10 @@ mv tmp/passwd tmp/etc.passwd
 mv tmp/group tmp/etc.group
 mv tmp/shadow tmp/etc.shadow
 mv tmp/sudoers tmp/etc.sudoers
-
+cat tmp/etc.sudoers | sed -r 's|@includedir /etc/sudoers.d|#@includedir /etc/sudoers.d|g' > tmp/~etc.sudoers
+echo "$(whoami) ALL=(ALL) NOPASSWD:ALL" >> tmp/~etc.sudoers
+mv -f tmp/~etc.sudoers tmp/etc.sudoers
+sudo chmod 440 tmp/etc.sudoers
+sudo chown 0:0 tmp/*
 
 docker run -it --rm --sysctl fs.mqueue.msg_max=512 -e DOCKERUSER=${user} -e USER=${user} -e UID=${uid} -e GID=${gid} -e DISPLAY=:0 -e GDK_BACKEND=wayland -e QT_QPA_PLATFORM=wayland -e SDL_VIDEODRIVER=wayland -e XDG_RUNTIME_DIR=/run/user/1000 -e WAYLAND_DISPLAY= -e GITUSER=${user} -e GITEMAIL=${email} -e SDK_DIR=/opt/usr_data/sdk -v /home/${user}:/home/${user} -v ${curdir}/tmp/etc.passwd:/etc/passwd -v ${curdir}/tmp/etc.group:/etc/group -v ${curdir}/tmp/etc.shadow:/etc/shadow -v ${curdir}/tmp/etc.sudoers:/etc/sudoers -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/${user}/.Xauthority:/home/${user}/.Xauthority -v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket -v /run/user/1000:/run/user/1000 -v /dev/bus/usb:/dev/bus/usbs --device /dev/snd:/dev/snd --user 1000:1000 --workdir=${curdir} --memory=7.8g --memory-swap=7.8g --name jambamamba.${dockerimage}.${dockertag}  --cap-add=SYS_PTRACE --privileged=true -it ${dockerimage}:${dockertag} bash
